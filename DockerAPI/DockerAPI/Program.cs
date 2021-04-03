ce9dfa07cd160e510efb0d83ec6a6b1b82081c4a
abbirku <abbir.ku@gmail.com>
2021-04-04 02:39:40 +0600

    Add dockerfile and update Program.cs for accessing port 80 according to docker need

--- DockerAPI/DockerAPI/Program.cs
+++ DockerAPI/DockerAPI/Program.cs
@@ -21,6 +21,7 @@ namespace DockerAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
                 });
     }
 }
