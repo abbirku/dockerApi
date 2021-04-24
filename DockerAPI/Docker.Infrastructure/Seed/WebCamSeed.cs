4b25c5c1284abd573f6a230ac2989e3d196e31bf
abbirku <abbir.ku@gmail.com>
2021-04-24 23:04:21 +0600

    Create ISeed, DataSeed & WebCamSeed and bind it to Ingrastructure module

---
+++ DockerAPI/Docker.Infrastructure/Seed/WebCamSeed.cs
@@ -0,0 +1,20 @@
+﻿using Docker.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docker.Infrastructure.Seed
+{
+    public class WebCamSeed : DataSeed
+    {
+        public WebCamSeed(ApiContext apiContext)
+            : base(apiContext)
+        { }
+
+        public override Task SeedAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
