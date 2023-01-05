989400c6c86d484bc94c5a1cbeffec9f95a43bf0
Md. Shams Wadud <abbir.ku@gmail.com>
2023-01-06 02:05:42 +0600

    Move IRepository, ISeed and IUnitOfWork under same file

--- DockerAPI/Docker.Core/UnitOfWork.cs
+++ DockerAPI/Docker.Core/UnitOfWork.cs
@@ -4,6 +4,13 @@ using System.Threading.Tasks;
 
 namespace Docker.Core
 {
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+    }
+
     public class UnitOfWork : IUnitOfWork
     {
         protected readonly DbContext _dbContext;
